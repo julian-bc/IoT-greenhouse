e05bfe0a440a46d4bc3229eb391a18843e27e756
Julian Enrique <julianbuitragocharry2006@gmail.com>
2026-09-01 06:48:27 -0500

    initial commit

---
+++ src/test/java/co/edu/usco/iotgreenhouse/IoTGreenhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.usco.iotgreenhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoTGreenhouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
